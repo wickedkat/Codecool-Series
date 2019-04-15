1d978df22876b4c202deb6f0fb9f45a614a2760d
Kat <kat.m.borowska@gmail.com>
2019-04-15 23:33:29 +0200

    add season_id to episodes query

--- database/queries/database_shows.py
+++ database/queries/database_shows.py
@@ -102,6 +102,7 @@ def get_episodes_by_show_and_season(cursor, season_id):
     cursor.execute("""
             SELECT 
                 seasons.title as season_title,
+                seasons.id as season_id,
                 episodes.id as episode_id,
                 to_char(episode_number, '999') as episode_number,
                 episodes.title as episode_title,
