5bbddb797bfc020093f04569e44f4c9b3eb7f86a
Kat <kat.m.borowska@gmail.com>
2019-04-15 20:12:29 +0200

    add table_mainpage.js/getDataMainPage()

---
+++ static/scripts/table_mainpage.js
@@ -0,0 +1,46 @@
+let shows;
+let show;
+let tableBody;
+let i;
+let Start;
+let End;
+
+mainpage = {
+
+
+    getDataMainPage: function getDataMainPage() {
+        let dataRequest = new XMLHttpRequest();
+        dataRequest.open('GET', '/shows');
+        dataRequest.onloadstart = dom.showLoadingModal();
+        dataRequest.onloadend = function () {
+            dom.hideLoadingModal();
+            shows = JSON.parse(dataRequest.response);
+            tableBody = document.getElementById("shows_mainpage");
+            if (tableBody.hasChildNodes()) {
+                this.deleteRows()
+            }
+            this.createTable(shows, 0, 15);
+            Start = 0;
+            End = 15
+
+        };
+        dataRequest.send();
+    }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
