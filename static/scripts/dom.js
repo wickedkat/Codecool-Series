41b343adc5389f500c410e855468e649f26ce0e1
Kat <kat.m.borowska@gmail.com>
2019-04-15 20:28:23 +0200

    add dom. LoadingModal functions

--- static/scripts/dom.js
+++ static/scripts/dom.js
@@ -2,7 +2,14 @@ let dom = {
     deleteRows: function deleteRows(tableBody){
         tableBody.innerHTML = '';
         return tableBody
-    }
+    },
+
+    showLoadingModal: function showLoadingModal() {
+        document.getElementById('loading-modal').style.display = "flex";
+    },
+    hideLoadingModal: function hideLoadingModal() {
+        document.getElementById('loading-modal').style.display = "none";
+    },
 
 };
 
