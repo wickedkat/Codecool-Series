90f229a9db5b44d527b87bb24dc0bbb8d7cf1db1
Kat <kat.m.borowska@gmail.com>
2019-04-15 21:57:41 +0200

    add seasons formatting function

--- logic/data_handler/data_format.py
+++ logic/data_handler/data_format.py
@@ -2,12 +2,11 @@ from database.queries import database_shows
 
 from logic.data_handler import data_validation
 
-def get_episodes_by_show_and_season(seasons):
+def get_episodes_by_show_and_season(seasons):  #function adding object['episodes'] within season objects
     for season in seasons:
         seasonId = season['season_id']
         episodes = database_shows.get_episodes_by_show_and_season(seasonId)
         season['episodes'] = episodes
     return seasons
-
 
 
