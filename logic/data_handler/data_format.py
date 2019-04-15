c70859dc91b7dd7fa7f83d280501d8faae92b3c0
Kat <kat.m.borowska@gmail.com>
2019-04-15 21:25:56 +0200

    add get_episodes_by_show_and_season()

--- logic/data_handler/data_format.py
+++ logic/data_handler/data_format.py
@@ -2,5 +2,12 @@ from database.queries import database_shows
 
 from logic.data_handler import data_validation
 
+def get_episodes_by_show_and_season(seasons):
+    for season in seasons:
+        seasonId = season['season_id']
+        episodes = database_shows.get_episodes_by_show_and_season(seasonId)
+        season['episodes'] = episodes
+    return seasons
+
 
 
