8bdd800a746d475aade3f60812f0bb590744c2b5
Kat <kat.m.borowska@gmail.com>
2019-04-15 20:14:33 +0200

    add createRow(), deleteRows()

--- static/scripts/table_mainpage.js
+++ static/scripts/table_mainpage.js
@@ -33,7 +33,26 @@ mainpage = {
             this.createRow(show)
         }
     },
+    createRow: function createRow(show) {
+        let row = document.createElement('tr');
+        tableBody.appendChild(row);
+        row.innerHTML = '<td>' + this.addLinkToShowTitle(show.id, show.title) + '</td>' +
+            '<td>' + show.year + '</td>' +
+            '<td>' + show.runtime + '</td>' +
+            '<td>' + this.limitGenres(show.genre) + '</td>' +
+            '<td>' + show.rating + '</td>' +
+            '<td>' + this.createTrailerIcon(show.trailer) + '</td>' +
+            '<td>' + this.createHomepageIcon(show.homepage) + '</td>' +
+            '<td>' + this.seasonsHandler(show.seasons_titles, show) + '</td>' +
+            '<td class="action-column"><button type="button" class="icon-button"><i class="fa fa-edit fa-fw"></i></button><button type="button" class="icon-button"><i class="fa fa-trash fa-fw"></i></button></td>'
+        this.createSeasonsModalUnderButton(show)
 
+    },
+
+    deleteRows: function deleteRows() {
+        tableBody.innerHTML = '';
+        return tableBody
+    },
 
 };
 
